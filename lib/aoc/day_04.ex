bcad3c9521a2e5d5c5349a1e1a5b8227217d92cc
Craig S. Cottingham <craig@cottingham.net>
2019-12-03 22:12:00 -0600

    Skeleton files for day 04

---
+++ lib/aoc/day_04.ex
@@ -0,0 +1,16 @@
+defmodule AoC.Day04 do
+  @moduledoc false
+
+  def part_1 do
+    # "data/day03-input.txt"
+    # |> load_paths_from_file()
+    # |> closest_intersection()
+    # |> manhattan_distance({0, 0})
+  end
+
+  def part_2 do
+    # "data/day03-input.txt"
+    # |> load_paths_from_file()
+    # |> shortest_path()
+  end
+end
