2b2895500c445fb572f88e12e6fd8c1a021ab2c8
Craig S. Cottingham <craig@cottingham.net>
2019-12-05 07:51:04 -0600

    Skeleton files for day 05

---
+++ lib/aoc/day_05.ex
@@ -0,0 +1,9 @@
+defmodule AoC.Day05 do
+  @moduledoc false
+
+  def part_1 do
+  end
+
+  def part_2 do
+  end
+end
---
+++ spec/day05_spec.exs
@@ -0,0 +1,8 @@
+defmodule AoC.Day05.Spec do
+  @moduledoc false
+
+  use ESpec
+
+  describe "sanity checks" do
+  end
+end
