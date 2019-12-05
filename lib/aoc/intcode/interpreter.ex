da503c27b015d861bcfa044a367a5a44da9d3d68
Craig S. Cottingham <craig@cottingham.net>
2019-12-05 15:21:20 -0600

    Add dependency on exprintf

--- lib/aoc/intcode/interpreter.ex
+++ lib/aoc/intcode/interpreter.ex
@@ -14,6 +14,10 @@ defmodule AoC.Intcode.Interpreter do
       {:halt, %{memory: mem}} ->
         mem
 
+      {:invalid_opcode, opcode, %{memory: mem, ip: ip}} ->
+        IO.puts("invalid opcode (#{opcode}) at position #{ip}: #{inspect(mem)}")
+        nil
+
       {:error, %{memory: mem, ip: ip}} ->
         IO.puts("error at position #{ip}: #{inspect(mem)}")
         nil
@@ -38,55 +42,162 @@ defmodule AoC.Intcode.Interpreter do
   defp get_value(_memory, value, :immediate), do: value
   defp get_value(memory, address, :position), do: Memory.read(memory, address)
 
+  # add
+  defp execute(
+         %{"instruction" => "01", "mode_1" => mode_1, "mode_2" => mode_2},
+         %{memory: memory, ip: ip} = state
+       ) do
+    param_1 = Memory.read(memory, ip + 1)
+    param_2 = Memory.read(memory, ip + 2)
+    dest = Memory.read(memory, ip + 3)
+
+    value_1 = get_value(memory, param_1, get_mode(mode_1))
+    value_2 = get_value(memory, param_2, get_mode(mode_2))
+    result = value_1 + value_2
+
+    step(%{state | memory: Memory.write(memory, dest, result), ip: ip + 4})
+  end
+
+  # multiply
+  defp execute(
+         %{"instruction" => "02", "mode_1" => mode_1, "mode_2" => mode_2},
+         %{memory: memory, ip: ip} = state
+       ) do
+    param_1 = Memory.read(memory, ip + 1)
+    param_2 = Memory.read(memory, ip + 2)
+    dest = Memory.read(memory, ip + 3)
+
+    value_1 = get_value(memory, param_1, get_mode(mode_1))
+    value_2 = get_value(memory, param_2, get_mode(mode_2))
+    result = value_1 * value_2
+
+    step(%{state | memory: Memory.write(memory, dest, result), ip: ip + 4})
+  end
+
+  # input
+  defp execute(%{"instruction" => "03"}, %{memory: memory, ip: ip} = state) do
+    dest = Memory.read(memory, ip + 1)
+
+    value = state.input_fn.()
+
+    step(%{state | memory: Memory.write(memory, dest, value), ip: ip + 2})
+  end
+
+  # output
+  defp execute(%{"instruction" => "04", "mode_1" => mode}, %{memory: memory, ip: ip} = state) do
+    param = Memory.read(memory, ip + 1)
+
+    value = get_value(memory, param, get_mode(mode))
+    state.output_fn.(value)
+
+    step(%{state | ip: ip + 2})
+  end
+
+  # jump-if-true
+  defp execute(
+         %{"instruction" => "05", "mode_1" => mode_1, "mode_2" => mode_2},
+         %{memory: memory, ip: ip} = state
+       ) do
+    param_1 = Memory.read(memory, ip + 1)
+    param_2 = Memory.read(memory, ip + 2)
+
+    value_1 = get_value(memory, param_1, get_mode(mode_1))
+    value_2 = get_value(memory, param_2, get_mode(mode_2))
+
+    new_ip =
+      if value_1 == 1 do
+        value_2
+      else
+        ip + 3
+      end
+
+    step(%{state | ip: new_ip})
+  end
+
+  # jump-if-false
+  defp execute(
+         %{"instruction" => "06", "mode_1" => mode_1, "mode_2" => mode_2},
+         %{memory: memory, ip: ip} = state
+       ) do
+    param_1 = Memory.read(memory, ip + 1)
+    param_2 = Memory.read(memory, ip + 2)
+
+    value_1 = get_value(memory, param_1, get_mode(mode_1))
+    value_2 = get_value(memory, param_2, get_mode(mode_2))
+
+    new_ip =
+      if value_1 == 0 do
+        value_2
+      else
+        ip + 3
+      end
+
+    step(%{state | ip: new_ip})
+  end
+
+  # less than
+  defp execute(
+         %{"instruction" => "07", "mode_1" => mode_1, "mode_2" => mode_2},
+         %{memory: memory, ip: ip} = state
+       ) do
+    param_1 = Memory.read(memory, ip + 1)
+    param_2 = Memory.read(memory, ip + 2)
+    dest = Memory.read(memory, ip + 3)
+
+    value_1 = get_value(memory, param_1, get_mode(mode_1))
+    value_2 = get_value(memory, param_2, get_mode(mode_2))
+
+    result =
+      if value_1 < value_2 do
+        1
+      else
+        0
+      end
+
+    step(%{state | memory: Memory.write(memory, dest, result), ip: ip + 4})
+  end
+
+  # equal
+  defp execute(
+         %{"instruction" => "08", "mode_1" => mode_1, "mode_2" => mode_2},
+         %{memory: memory, ip: ip} = state
+       ) do
+    param_1 = Memory.read(memory, ip + 1)
+    param_2 = Memory.read(memory, ip + 2)
+    dest = Memory.read(memory, ip + 3)
+
+    value_1 = get_value(memory, param_1, get_mode(mode_1))
+    value_2 = get_value(memory, param_2, get_mode(mode_2))
+
+    result =
+      if value_1 == value_2 do
+        1
+      else
+        0
+      end
+
+    step(%{state | memory: Memory.write(memory, dest, result), ip: ip + 4})
+  end
+
+  # exit
+  defp execute(%{"instruction" => "99"}, state) do
+    {:halt, state}
+  end
+
+  # invalid opcode
+  defp execute(%{"instruction" => instruction}, state) do
+    {:invalid_opcode, instruction, state}
+  end
+
+  # unspecified error
+  defp execute(_, state) do
+    {:error, state}
+  end
+
   defp step(%{memory: memory, ip: ip} = state) do
-    case decode(Memory.read(memory, ip)) do
-      # add
-      %{"instruction" => "01", "mode_1" => mode_1, "mode_2" => mode_2} ->
-        param_1 = Memory.read(memory, ip + 1)
-        param_2 = Memory.read(memory, ip + 2)
-        dest = Memory.read(memory, ip + 3)
-
-        value_1 = get_value(memory, param_1, get_mode(mode_1))
-        value_2 = get_value(memory, param_2, get_mode(mode_2))
-        result = value_1 + value_2
-
-        step(%{state | memory: Memory.write(memory, dest, result), ip: ip + 4})
-
-      # multiply
-      %{"instruction" => "02", "mode_1" => mode_1, "mode_2" => mode_2} ->
-        param_1 = Memory.read(memory, ip + 1)
-        param_2 = Memory.read(memory, ip + 2)
-        dest = Memory.read(memory, ip + 3)
-
-        value_1 = get_value(memory, param_1, get_mode(mode_1))
-        value_2 = get_value(memory, param_2, get_mode(mode_2))
-        result = value_1 * value_2
-
-        step(%{state | memory: Memory.write(memory, dest, result), ip: ip + 4})
-
-      # input
-      %{"instruction" => "03"} ->
-        dest = Memory.read(memory, ip + 1)
-
-        value = state.input_fn.()
-
-        step(%{state | memory: Memory.write(memory, dest, value), ip: ip + 2})
-
-      # output
-      %{"instruction" => "04", "mode_1" => mode} ->
-        param = Memory.read(memory, ip + 1)
-
-        value = get_value(memory, param, get_mode(mode))
-        state.output_fn.(value)
-
-        step(%{state | ip: ip + 2})
-
-      # exit
-      %{"instruction" => "99"} ->
-        {:halt, state}
-
-      _ ->
-        {:error, state}
-    end
+    memory
+    |> Memory.read(ip)
+    |> decode()
+    |> execute(state)
   end
 end
--- spec/aoc/intcode/interpreter_spec.exs
+++ spec/aoc/intcode/interpreter_spec.exs
@@ -3,48 +3,63 @@ defmodule AoC.Intcode.Interpreter.Spec do
 
   use ESpec
 
-  alias AoC.Intcode.Interpreter
+  alias AoC.Intcode.{Interpreter, Memory}
 
   example_group "run/1" do
-    context "parameter direct mode" do
-      it "tests opcode 1 (add)" do
+    context "opcode 1 (add)" do
+      it "tests position mode" do
         Interpreter.initialize()
         |> Interpreter.set_memory([1, 0, 0, 0, 99])
         |> Interpreter.run()
+        |> Memory.read(0)
         |> expect()
-        |> to(eq([2, 0, 0, 0, 99]))
-
-        Interpreter.initialize()
-        |> Interpreter.set_memory([1, 5, 6, 7, 99, 2, 3, 0])
-        |> Interpreter.run()
-        |> expect()
-        |> to(eq([1, 5, 6, 7, 99, 2, 3, 5]))
+        |> to(eq(2))
       end
 
-      it "tests opcode 2 (multiply)" do
+      it "tests immediate mode" do
+        Interpreter.initialize()
+        |> Interpreter.set_memory([1101, 5, 6, 5, 99, 0])
+        |> Interpreter.run()
+        |> Memory.read(5)
+        |> expect()
+        |> to(eq(11))
+      end
+    end
+
+    context "opcode 2 (multiply)" do
+      it "tests position mode" do
         Interpreter.initialize()
         |> Interpreter.set_memory([2, 3, 0, 3, 99])
         |> Interpreter.run()
+        |> Memory.read(3)
         |> expect()
-        |> to(eq([2, 3, 0, 6, 99]))
-
-        Interpreter.initialize()
-        |> Interpreter.set_memory([2, 5, 6, 7, 99, 2, 3, 0])
-        |> Interpreter.run()
-        |> expect()
-        |> to(eq([2, 5, 6, 7, 99, 2, 3, 6]))
+        |> to(eq(6))
       end
 
-      it "tests opcode 3 (input)" do
+      it "tests immediate mode" do
+        Interpreter.initialize()
+        |> Interpreter.set_memory([1102, 5, 6, 5, 99, 0])
+        |> Interpreter.run()
+        |> Memory.read(5)
+        |> expect()
+        |> to(eq(30))
+      end
+    end
+
+    context "opcode 3 (input)" do
+      it do
         Interpreter.initialize()
         |> Interpreter.set_memory([3, 3, 99, 0])
         |> Interpreter.set_input(fn -> 1 end)
         |> Interpreter.run()
+        |> Memory.read(3)
         |> expect()
-        |> to(eq([3, 3, 99, 1]))
+        |> to(eq(1))
       end
+    end
 
-      it "tests opcode 4 (output)" do
+    context "opcode 4 (output)" do
+      it "tests position mode" do
         {:ok, agent} = Agent.start_link(fn -> nil end)
         output_fn = fn value -> Agent.update(agent, fn _ -> value end) end
 
@@ -52,10 +67,392 @@ defmodule AoC.Intcode.Interpreter.Spec do
         |> Interpreter.set_memory([4, 3, 99, 2])
         |> Interpreter.set_output(output_fn)
         |> Interpreter.run()
-        |> expect()
-        |> to(eq([4, 3, 99, 2]))
 
         expect(Agent.get(agent, fn value -> value end) |> to(eq(2)))
+
+        Agent.stop(agent)
+      end
+
+      it "tests immediate mode" do
+        {:ok, agent} = Agent.start_link(fn -> nil end)
+        output_fn = fn value -> Agent.update(agent, fn _ -> value end) end
+
+        Interpreter.initialize()
+        |> Interpreter.set_memory([104, 3, 99, 2])
+        |> Interpreter.set_output(output_fn)
+        |> Interpreter.run()
+
+        expect(Agent.get(agent, fn value -> value end) |> to(eq(3)))
+
+        Agent.stop(agent)
+      end
+    end
+
+    context "opcode 5 (jump-if-true)" do
+      it "tests happy path (input == 2), position mode" do
+        input_fn = fn -> 1 end
+        output_fn = fn value -> value end
+
+        Interpreter.initialize()
+        |> Interpreter.set_memory([3, 13, 5, 13, 14, 1101, 0, 0, 12, 4, 12, 99, 1, -1, 9])
+        |> Interpreter.set_input(input_fn)
+        |> Interpreter.set_output(output_fn)
+        |> Interpreter.run()
+        |> Memory.read(12)
+        |> expect()
+        |> to(eq(1))
+      end
+
+      it "tests happy path (input == 2), immediate mode" do
+        input_fn = fn -> 1 end
+        output_fn = fn value -> value end
+
+        Interpreter.initialize()
+        |> Interpreter.set_memory([3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1])
+        |> Interpreter.set_input(input_fn)
+        |> Interpreter.set_output(output_fn)
+        |> Interpreter.run()
+        |> Memory.read(12)
+        |> expect()
+        |> to(eq(1))
+      end
+
+      it "tests unhappy path (input == 0), immediate mode" do
+        input_fn = fn -> 0 end
+        output_fn = fn value -> value end
+
+        Interpreter.initialize()
+        |> Interpreter.set_memory([3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1])
+        |> Interpreter.set_input(input_fn)
+        |> Interpreter.set_output(output_fn)
+        |> Interpreter.run()
+        |> Memory.read(12)
+        |> expect()
+        |> to(eq(0))
+      end
+    end
+
+    context "opcode 6 (jump-if-false)" do
+      it "tests happy path (input == 0), position mode" do
+        input_fn = fn -> 0 end
+        output_fn = fn value -> value end
+
+        Interpreter.initialize()
+        |> Interpreter.set_memory([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9])
+        |> Interpreter.set_input(input_fn)
+        |> Interpreter.set_output(output_fn)
+        |> Interpreter.run()
+        |> Memory.read(13)
+        |> expect()
+        |> to(eq(0))
+      end
+
+      it "tests happy path (input == 0), immediate mode" do
+        input_fn = fn -> 0 end
+        output_fn = fn value -> value end
+
+        Interpreter.initialize()
+        |> Interpreter.set_memory([3, 3, 1106, -1, 9, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9])
+        |> Interpreter.set_input(input_fn)
+        |> Interpreter.set_output(output_fn)
+        |> Interpreter.run()
+        |> Memory.read(13)
+        |> expect()
+        |> to(eq(0))
+      end
+
+      it "tests unhappy path (input == 2), immediate mode" do
+        input_fn = fn -> 2 end
+        output_fn = fn value -> value end
+
+        Interpreter.initialize()
+        |> Interpreter.set_memory([3, 3, 1106, -1, 9, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9])
+        |> Interpreter.set_input(input_fn)
+        |> Interpreter.set_output(output_fn)
+        |> Interpreter.run()
+        |> Memory.read(13)
+        |> expect()
+        |> to(eq(1))
+      end
+    end
+
+    context "opcode 7 (less than)" do
+      it "tests happy path (input < 8), position mode" do
+        input_fn = fn -> 0 end
+        output_fn = fn value -> value end
+
+        Interpreter.initialize()
+        |> Interpreter.set_memory([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8])
+        |> Interpreter.set_input(input_fn)
+        |> Interpreter.set_output(output_fn)
+        |> Interpreter.run()
+        |> Memory.read(9)
+        |> expect()
+        |> to(eq(1))
+      end
+
+      it "tests happy path (input < 8), immediate mode" do
+        input_fn = fn -> 0 end
+        output_fn = fn value -> value end
+
+        Interpreter.initialize()
+        |> Interpreter.set_memory([3, 3, 1107, -1, 8, 3, 4, 3, 99])
+        |> Interpreter.set_input(input_fn)
+        |> Interpreter.set_output(output_fn)
+        |> Interpreter.run()
+        |> Memory.read(3)
+        |> expect()
+        |> to(eq(1))
+      end
+
+      it "tests unhappy path (input >= 8)" do
+        input_fn = fn -> 9 end
+        output_fn = fn value -> value end
+
+        Interpreter.initialize()
+        |> Interpreter.set_memory([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8])
+        |> Interpreter.set_input(input_fn)
+        |> Interpreter.set_output(output_fn)
+        |> Interpreter.run()
+        |> Memory.read(9)
+        |> expect()
+        |> to(eq(0))
+      end
+    end
+
+    context "opcode 8 (equals)" do
+      it "tests happy path (input == 8), position mode" do
+        input_fn = fn -> 8 end
+        output_fn = fn value -> value end
+
+        Interpreter.initialize()
+        |> Interpreter.set_memory([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8])
+        |> Interpreter.set_input(input_fn)
+        |> Interpreter.set_output(output_fn)
+        |> Interpreter.run()
+        |> Memory.read(9)
+        |> expect()
+        |> to(eq(1))
+      end
+
+      it "tests happy path (input == 8), immediate mode" do
+        input_fn = fn -> 8 end
+        output_fn = fn value -> value end
+
+        Interpreter.initialize()
+        |> Interpreter.set_memory([3, 3, 1108, -1, 8, 3, 4, 3, 99])
+        |> Interpreter.set_input(input_fn)
+        |> Interpreter.set_output(output_fn)
+        |> Interpreter.run()
+        |> Memory.read(3)
+        |> expect()
+        |> to(eq(1))
+      end
+
+      it "tests unhappy path (input != 8)" do
+        input_fn = fn -> 1 end
+        output_fn = fn value -> value end
+
+        Interpreter.initialize()
+        |> Interpreter.set_memory([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8])
+        |> Interpreter.set_input(input_fn)
+        |> Interpreter.set_output(output_fn)
+        |> Interpreter.run()
+        |> Memory.read(9)
+        |> expect()
+        |> to(eq(0))
+      end
+    end
+
+    context "a complex example" do
+      it "tests input < 8" do
+        {:ok, agent} = Agent.start_link(fn -> nil end)
+        input_fn = fn -> 7 end
+        output_fn = fn value -> Agent.update(agent, fn _ -> value end) end
+
+        Interpreter.initialize()
+        |> Interpreter.set_memory([
+          3,
+          21,
+          1008,
+          21,
+          8,
+          20,
+          1005,
+          20,
+          22,
+          107,
+          8,
+          21,
+          20,
+          1006,
+          20,
+          31,
+          1106,
+          0,
+          36,
+          98,
+          0,
+          0,
+          1002,
+          21,
+          125,
+          20,
+          4,
+          20,
+          1105,
+          1,
+          46,
+          104,
+          999,
+          1105,
+          1,
+          46,
+          1101,
+          1000,
+          1,
+          20,
+          4,
+          20,
+          1105,
+          1,
+          46,
+          98,
+          99
+        ])
+        |> Interpreter.set_input(input_fn)
+        |> Interpreter.set_output(output_fn)
+        |> Interpreter.run()
+
+        expect(Agent.get(agent, fn value -> value end) |> to(eq(999)))
+
+        Agent.stop(agent)
+      end
+
+      it "tests input == 8" do
+        {:ok, agent} = Agent.start_link(fn -> nil end)
+        input_fn = fn -> 8 end
+        output_fn = fn value -> Agent.update(agent, fn _ -> value end) end
+
+        Interpreter.initialize()
+        |> Interpreter.set_memory([
+          3,
+          21,
+          1008,
+          21,
+          8,
+          20,
+          1005,
+          20,
+          22,
+          107,
+          8,
+          21,
+          20,
+          1006,
+          20,
+          31,
+          1106,
+          0,
+          36,
+          98,
+          0,
+          0,
+          1002,
+          21,
+          125,
+          20,
+          4,
+          20,
+          1105,
+          1,
+          46,
+          104,
+          999,
+          1105,
+          1,
+          46,
+          1101,
+          1000,
+          1,
+          20,
+          4,
+          20,
+          1105,
+          1,
+          46,
+          98,
+          99
+        ])
+        |> Interpreter.set_input(input_fn)
+        |> Interpreter.set_output(output_fn)
+        |> Interpreter.run()
+
+        expect(Agent.get(agent, fn value -> value end) |> to(eq(1000)))
+
+        Agent.stop(agent)
+      end
+
+      it "tests input == 9" do
+        {:ok, agent} = Agent.start_link(fn -> nil end)
+        input_fn = fn -> 9 end
+        output_fn = fn value -> Agent.update(agent, fn _ -> value end) end
+
+        Interpreter.initialize()
+        |> Interpreter.set_memory([
+          3,
+          21,
+          1008,
+          21,
+          8,
+          20,
+          1005,
+          20,
+          22,
+          107,
+          8,
+          21,
+          20,
+          1006,
+          20,
+          31,
+          1106,
+          0,
+          36,
+          98,
+          0,
+          0,
+          1002,
+          21,
+          125,
+          20,
+          4,
+          20,
+          1105,
+          1,
+          46,
+          104,
+          999,
+          1105,
+          1,
+          46,
+          1101,
+          1000,
+          1,
+          20,
+          4,
+          20,
+          1105,
+          1,
+          46,
+          98,
+          99
+        ])
+        |> Interpreter.set_input(input_fn)
+        |> Interpreter.set_output(output_fn)
+        |> Interpreter.run()
+
+        expect(Agent.get(agent, fn value -> value end) |> to(eq(1001)))
 
         Agent.stop(agent)
       end
