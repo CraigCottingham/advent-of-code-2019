22c95fcd55a3d0c1201f363a2e2e9f9253f78538
Craig S. Cottingham <craig@cottingham.net>
2019-12-05 08:43:36 -0600

    Simplify Intcode interpreter

--- lib/aoc/intcode/interpreter.ex
+++ lib/aoc/intcode/interpreter.ex
@@ -1,52 +1,47 @@
 defmodule AoC.Intcode.Interpreter do
   @moduledoc false
 
-  # alias AoC.Intcode.Memory
+  def run(memory) do
+    state = %{memory: memory, ip: 0}
 
-  def run(mem) do
-    # mem =
-    #   program
-    #   |> Memory.set_noun(noun)
-    #   |> Memory.set_verb(verb)
-
-    case step({:cont, mem, 0}) do
-      {:halt, mem} ->
+    case step(state) do
+      {:halt, %{memory: mem}} ->
         mem
 
-      {:error, mem, ip} ->
+      {:error, %{memory: mem, ip: ip}} ->
         IO.puts("error at position #{ip}: #{inspect(mem)}")
         nil
     end
   end
 
-  defp step({:cont, program, ip}) do
-    case Enum.at(program, ip) do
+  defp step(%{memory: memory, ip: ip} = state) do
+    case Enum.at(memory, ip) do
       # add
       1 ->
-        source_1 = Enum.at(program, ip + 1)
-        source_2 = Enum.at(program, ip + 2)
-        dest = Enum.at(program, ip + 3)
+        source_1 = Enum.at(memory, ip + 1)
+        source_2 = Enum.at(memory, ip + 2)
+        dest = Enum.at(memory, ip + 3)
 
-        addend_1 = Enum.at(program, source_1)
-        addend_2 = Enum.at(program, source_2)
-        step({:cont, List.replace_at(program, dest, addend_1 + addend_2), ip + 4})
+        value_1 = Enum.at(memory, source_1)
+        value_2 = Enum.at(memory, source_2)
+        step(%{state | memory: List.replace_at(memory, dest, value_1 + value_2), ip: ip + 4})
 
       # multiply
       2 ->
-        source_1 = Enum.at(program, ip + 1)
-        source_2 = Enum.at(program, ip + 2)
-        dest = Enum.at(program, ip + 3)
+        source_1 = Enum.at(memory, ip + 1)
+        source_2 = Enum.at(memory, ip + 2)
+        dest = Enum.at(memory, ip + 3)
 
-        addend_1 = Enum.at(program, source_1)
-        addend_2 = Enum.at(program, source_2)
-        step({:cont, List.replace_at(program, dest, addend_1 * addend_2), ip + 4})
+        value_1 = Enum.at(memory, source_1)
+        value_2 = Enum.at(memory, source_2)
+        step(%{state | memory: List.replace_at(memory, dest, value_1 * value_2), ip: ip + 4})
 
       # exit
       99 ->
-        {:halt, program}
+        {:halt, state}
 
       _ ->
-        {:error, program, ip}
+        {:error, state}
     end
   end
 end
