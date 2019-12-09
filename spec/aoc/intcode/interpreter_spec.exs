8bfa16ef4ae9dad65733e2a5d78bc459ffae20d6
Craig S. Cottingham <craig@cottingham.net>
2019-12-09 12:35:27 -0600

    Add relative mode and memory auto-expansion

--- spec/aoc/intcode/interpreter_spec.exs
+++ spec/aoc/intcode/interpreter_spec.exs
@@ -5,6 +5,9 @@ defmodule AoC.Intcode.Interpreter.Spec do
 
   alias AoC.Intcode.{Interpreter, Memory}
 
+  ## TODO: add specs for parameter modes
+  ## TODO: add specs for large integer support
+
   example_group "start/1" do
     it do
       vm = Task.async(Interpreter, :initialize, [%{memory: [1, 0, 0, 0, 99]}])
@@ -45,6 +48,13 @@ defmodule AoC.Intcode.Interpreter.Spec do
         {:halt, %{state: :stopped, memory: memory}} = Task.await(vm)
         expect(Memory.read(memory, 5)) |> to(eq(11))
       end
+
+      it "tests relative mode" do
+        memory = [109, 1, 22201, 2, 3, -1, 99]
+        vm = Task.async(Interpreter, :initialize, [%{state: :running, memory: memory}])
+        {:halt, %{state: :stopped, memory: memory}} = Task.await(vm)
+        expect(Memory.read(memory, 0)) |> to(eq(5))
+      end
     end
 
     context "opcode 2 (multiply)" do
@@ -61,15 +71,30 @@ defmodule AoC.Intcode.Interpreter.Spec do
         {:halt, %{state: :stopped, memory: memory}} = Task.await(vm)
         expect(Memory.read(memory, 5)) |> to(eq(30))
       end
+
+      it "tests relative mode" do
+        memory = [109, 1, 22202, 2, 3, -1, 99]
+        vm = Task.async(Interpreter, :initialize, [%{state: :running, memory: memory}])
+        {:halt, %{state: :stopped, memory: memory}} = Task.await(vm)
+        expect(Memory.read(memory, 0)) |> to(eq(6))
+      end
     end
 
     context "opcode 3 (input)" do
-      it do
+      it "tests position mode" do
         memory = [3, 3, 99, 0]
         vm = Task.async(Interpreter, :initialize, [%{state: :running, memory: memory}])
         send(vm.pid, 1)
         {:halt, %{state: :stopped, memory: memory}} = Task.await(vm)
         expect(Memory.read(memory, 3)) |> to(eq(1))
+      end
+
+      it "tests relative mode" do
+        memory = [109, 1, 203, 4, 99, 0]
+        vm = Task.async(Interpreter, :initialize, [%{state: :running, memory: memory}])
+        send(vm.pid, 1)
+        {:halt, %{state: :stopped, memory: memory}} = Task.await(vm)
+        expect(Memory.read(memory, 5)) |> to(eq(1))
       end
     end
 
@@ -106,6 +131,24 @@ defmodule AoC.Intcode.Interpreter.Spec do
         {:halt, %{state: :stopped}} = Task.await(vm)
 
         expect(Agent.get(agent, fn value -> value end)) |> to(eq(3))
+
+        Agent.stop(agent)
+      end
+
+      it "tests relative mode" do
+        {:ok, agent} = Agent.start_link(fn -> nil end)
+        output_fn = fn value -> Agent.update(agent, fn _ -> value end) end
+
+        memory = [109, 4, 204, 1, 99, 2]
+
+        vm =
+          Task.async(Interpreter, :initialize, [
+            %{state: :running, memory: memory, output_fn: output_fn}
+          ])
+
+        {:halt, %{state: :stopped}} = Task.await(vm)
+
+        expect(Agent.get(agent, fn value -> value end)) |> to(eq(2))
 
         Agent.stop(agent)
       end
@@ -148,6 +191,26 @@ defmodule AoC.Intcode.Interpreter.Spec do
 
         expect(Agent.get(agent, fn value -> value end)) |> to(eq(1))
         expect(Memory.read(memory, 12)) |> to(eq(1))
+      end
+
+      it "tests happy path (input == 2), relative mode" do
+        {:ok, agent} = Agent.start_link(fn -> nil end)
+        output_fn = fn value -> Agent.update(agent, fn _ -> value end) end
+
+        memory = [109, 1, 3, 15, 2205, 14, 15, 1101, 0, 0, 12, 4, 15, 99, 1, -1, 11]
+
+        vm =
+          Task.async(Interpreter, :initialize, [
+            %{state: :running, memory: memory, output_fn: output_fn}
+          ])
+
+        send(vm.pid, 1)
+        {:halt, %{state: :stopped, memory: memory}} = Task.await(vm)
+
+        expect(Agent.get(agent, fn value -> value end)) |> to(eq(1))
+        expect(Memory.read(memory, 15)) |> to(eq(1))
+
+        Agent.stop(agent)
       end
 
       it "tests unhappy path (input == 0), immediate mode" do
@@ -212,6 +275,26 @@ defmodule AoC.Intcode.Interpreter.Spec do
         Agent.stop(agent)
       end
 
+      it "tests happy path (input == 0), relative mode" do
+        {:ok, agent} = Agent.start_link(fn -> nil end)
+        output_fn = fn value -> Agent.update(agent, fn _ -> value end) end
+
+        memory = [109, 1, 3, 14, 2206, 13, 16, 1, 15, 16, 15, 4, 15, 99, -1, 0, 1, 11]
+
+        vm =
+          Task.async(Interpreter, :initialize, [
+            %{state: :running, memory: memory, output_fn: output_fn}
+          ])
+
+        send(vm.pid, 0)
+        {:halt, %{state: :stopped, memory: memory}} = Task.await(vm)
+
+        expect(Agent.get(agent, fn value -> value end)) |> to(eq(0))
+        expect(Memory.read(memory, 15)) |> to(eq(0))
+
+        Agent.stop(agent)
+      end
+
       it "tests unhappy path (input == 2), immediate mode" do
         {:ok, agent} = Agent.start_link(fn -> nil end)
         output_fn = fn value -> Agent.update(agent, fn _ -> value end) end
@@ -254,22 +337,42 @@ defmodule AoC.Intcode.Interpreter.Spec do
         Agent.stop(agent)
       end
 
-      it "tests happy path (input < 8), immediate mode" do
+      it "tests happy path (input < 9), immediate mode" do
         {:ok, agent} = Agent.start_link(fn -> nil end)
         output_fn = fn value -> Agent.update(agent, fn _ -> value end) end
 
-        memory = [3, 3, 1107, -1, 8, 3, 4, 3, 99]
+        memory = [3, 3, 1107, -1, 9, 3, 4, 3, 99]
 
         vm =
           Task.async(Interpreter, :initialize, [
             %{state: :running, memory: memory, output_fn: output_fn}
           ])
 
-        send(vm.pid, 7)
+        send(vm.pid, 8)
         {:halt, %{state: :stopped, memory: memory}} = Task.await(vm)
 
         expect(Agent.get(agent, fn value -> value end)) |> to(eq(1))
         expect(Memory.read(memory, 3)) |> to(eq(1))
+
+        Agent.stop(agent)
+      end
+
+      it "tests happy path (input < 10), relative mode" do
+        {:ok, agent} = Agent.start_link(fn -> nil end)
+        output_fn = fn value -> Agent.update(agent, fn _ -> value end) end
+
+        memory = [109, 1, 3, 11, 22207, 10, 11, 10, 4, 11, 99, -1, 10]
+
+        vm =
+          Task.async(Interpreter, :initialize, [
+            %{state: :running, memory: memory, output_fn: output_fn}
+          ])
+
+        send(vm.pid, 9)
+        {:halt, %{state: :stopped, memory: memory}} = Task.await(vm)
+
+        expect(Agent.get(agent, fn value -> value end)) |> to(eq(1))
+        expect(Memory.read(memory, 11)) |> to(eq(1))
 
         Agent.stop(agent)
       end
@@ -336,6 +439,26 @@ defmodule AoC.Intcode.Interpreter.Spec do
         Agent.stop(agent)
       end
 
+      it "tests happy path (input == 10), relative mode" do
+        {:ok, agent} = Agent.start_link(fn -> nil end)
+        output_fn = fn value -> Agent.update(agent, fn _ -> value end) end
+
+        memory = [109, 1, 3, 11, 22208, 10, 11, 10, 4, 11, 99, -1, 10]
+
+        vm =
+          Task.async(Interpreter, :initialize, [
+            %{state: :running, memory: memory, output_fn: output_fn}
+          ])
+
+        send(vm.pid, 10)
+        {:halt, %{state: :stopped, memory: memory}} = Task.await(vm)
+
+        expect(Agent.get(agent, fn value -> value end)) |> to(eq(1))
+        expect(Memory.read(memory, 11)) |> to(eq(1))
+
+        Agent.stop(agent)
+      end
+
       it "tests unhappy path (input != 8)" do
         {:ok, agent} = Agent.start_link(fn -> nil end)
         output_fn = fn value -> Agent.update(agent, fn _ -> value end) end
@@ -354,6 +477,15 @@ defmodule AoC.Intcode.Interpreter.Spec do
         expect(Memory.read(memory, 9)) |> to(eq(0))
 
         Agent.stop(agent)
+      end
+    end
+
+    context "opcode 9 (adjust relative base)" do
+      it do
+        memory = [9, 3, 99, 1]
+        vm = Task.async(Interpreter, :initialize, [%{state: :running, memory: memory}])
+        {:halt, %{state: :stopped, rp: rp}} = Task.await(vm)
+        expect(rp) |> to(eq(1))
       end
     end
 
